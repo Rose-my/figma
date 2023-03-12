a1d0845f1127f7256bf094fe44d09712656e2b60
Rose-my <amanda1000@ewhain.net>
2023-03-12 21:39:27 +0900

    first commit

---
+++ src/pages/Main1.js
@@ -0,0 +1,73 @@
+import styles from "./Main1.module.css";
+
+const Main1 = () => {
+  return (
+    <div className={styles.main1}>
+      <div className={styles.main1Child} />
+      <div className={styles.main1Item} />
+      <div className={styles.main1Inner} />
+      <div className={styles.seobinWonsNftContainer}>
+        <p className={styles.seobinWons}>
+          <span className={styles.span}>
+            <span className={styles.seobinWons2}>Seobin Won’s</span>
+          </span>
+          <span>
+            <span className={styles.span}>{` `}</span>
+          </span>
+        </p>
+        <p className={styles.nftCollection}>
+          <span>
+            <b>NFT Collection</b>
+          </span>
+        </p>
+      </div>
+      <div className={styles.rectangleDiv} />
+      <div className={styles.main1Child1} />
+      <div className={styles.main1Child2} />
+      <img className={styles.ellipseIcon} alt="" src="/ellipse-2@2x.png" />
+      <div className={styles.seobinWonParent}>
+        <b className={styles.seobinWon}>Seobin Won</b>
+        <div className={styles.wonSeobin}>@won_seobin_</div>
+      </div>
+      <div className={styles.mint}>MINT!</div>
+      <div className={styles.rdNft}>3rd NFT</div>
+      <img className={styles.homeIcon} alt="" src="/home.svg" />
+      <div className={styles.main1Child3} />
+      <div className={styles.main1Child4} />
+      <div className={styles.seobinWonsNftContainer1}>
+        <p className={styles.seobinWons}>
+          <span className={styles.span}>
+            <span className={styles.seobinWons2}>Seobin Won’s</span>
+          </span>
+          <span>
+            <span className={styles.span}>{` `}</span>
+          </span>
+        </p>
+        <p className={styles.nftCollection}>
+          <span>
+            <b>NFT Collection</b>
+          </span>
+        </p>
+      </div>
+      <img className={styles.vectorIcon} alt="" />
+      <img className={styles.main1Child5} alt="" src="/vector-3.svg" />
+      <div className={styles.tumbler}>Tumbler</div>
+      <div className={styles.nfts}>NFTs</div>
+      <div className={styles.div}>2</div>
+      <div className={styles.div1}>15</div>
+      <div className={styles.main1Child6} />
+      <img className={styles.main1Child7} alt="" src="/ellipse-3.svg" />
+      <img className={styles.walletIcon} alt="" src="/wallet@2x.png" />
+      <img className={styles.frameIcon} alt="" src="/frame.svg" />
+      <img className={styles.vectorIcon1} alt="" src="/vector.svg" />
+      <img className={styles.slrCameraIcon} alt="" src="/slr-camera@2x.png" />
+      <b className={styles.uploadedReceipt}>
+        <p className={styles.seobinWons}>Uploaded</p>
+        <p className={styles.receipt}>Receipt</p>
+      </b>
+      <img className={styles.frameIcon1} alt="" src="/frame1.svg" />
+    </div>
+  );
+};
+
+export default Main1;
